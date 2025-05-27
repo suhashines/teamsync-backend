b3cb80c349139796a4f0f24b48228cff25377522
Riyad <riyadsdp333@gmail.com>
2025-05-27 22:03:28 +0600

    get User:user

--- src/main/java/edu/teamsync/teamsync/controller/TaskController.java
+++ src/main/java/edu/teamsync/teamsync/controller/TaskController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/tasks")
+@RequestMapping("/tasks")
 public class TaskController {
 
     @Autowired
