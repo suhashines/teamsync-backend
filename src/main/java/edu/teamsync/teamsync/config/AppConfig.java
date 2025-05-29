1475ae3d7ffc0ae5caa8f43cf778e33dda1507bf
Riyad <riyadsdp333@gmail.com>
2025-05-30 00:22:37 +0600

    insertProjectsData

--- src/main/java/edu/teamsync/teamsync/config/AppConfig.java
+++ src/main/java/edu/teamsync/teamsync/config/AppConfig.java
@@ -1,43 +1,3 @@
-//package edu.teamsync.teamsync.config;
-//
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.http.SessionCreationPolicy;
-//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-//import org.springframework.security.crypto.password.PasswordEncoder;
-//import org.springframework.security.web.SecurityFilterChain;
-//import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
-//
-//import static org.springframework.security.config.Customizer.withDefaults;
-//
-//@Configuration
-//public class AppConfig {
-//    @Bean
-//    public SecurityFilterChain securityFilterChain(HttpSecurity http, JwtValidator jwtValidator) throws Exception {
-//        http
-//                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-//                .authorizeHttpRequests(auth -> auth
-//                        .requestMatchers("/api/tasks").authenticated()
-//                        .anyRequest().permitAll()
-//                )
-//                .addFilterBefore(jwtValidator, BasicAuthenticationFilter.class)
-//                .csrf(csrf -> csrf.disable())
-//                .cors(withDefaults());
-//
-//        return http.build();
-//    }
-//
-//    @Bean
-//    public JwtValidator jwtValidator(JwtConstant jwtConstant) {
-//        return new JwtValidator(jwtConstant);
-//    }
-//
-//    @Bean
-//    public PasswordEncoder passwordEncoder() {
-//        return new BCryptPasswordEncoder();
-//    }
-//}
 package edu.teamsync.teamsync.config;
 
 import org.springframework.context.annotation.Bean;
@@ -48,11 +8,9 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
-import org.springframework.web.cors.CorsConfigurationSource;
-
 import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import java.util.List;
 
