39eb3f16d51039213e42731044cbdf693cf8f34e
suhashines <suhasabdullah43@gmail.com>
2025-05-31 10:34:43 +0600

    fix:flyway version control

--- flyway/sql/V2__insertInitialData.sql
+++ flyway/sql/V2__insertInitialData.sql
@@ -5,10 +5,4 @@ VALUES
     ('riyad', 'riyad@gmail.com', '$2a$10$Zsk/ghxxz142M5/ZuVgjget5.APB7tzNlZKKO.Lq3FmdWS9JooCL2'),
     ('hossain', 'hossain@gmail.com', '$2a$10$GVZLuymconMfPbameqH/kOJ6r69ISqqa8VoqsuoBQpRfKkknOy6lW');
 
-INSERT INTO Projects (id, title, created_by, created_at) VALUES (1, 'Project Alpha', 1, '2025-05-26T17:24:00+06:00');
-INSERT INTO ProjectMembers (project_id, user_id, role) VALUES (1, 1, 'owner');
-INSERT INTO Tasks (id, title, status, project_id, assigned_to) VALUES (1, 'Design dashboard', 'in_progress', 1, 1);
-INSERT INTO Tasks (id, title, status, project_id, assigned_to) VALUES (2, 'Design dashboard', 'in_progress', 1, 2);
-
-
 
