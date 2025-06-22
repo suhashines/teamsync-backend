c1488d29854bba7faccb36ad977509530f7a217c
Riyad <riyadsdp333@gmail.com>
2025-06-22 13:44:35 +0600

    message updates issue solved

--- src/main/java/edu/teamsync/teamsync/dto/messageDTO/MessageUpdateDTO.java
+++ src/main/java/edu/teamsync/teamsync/dto/messageDTO/MessageUpdateDTO.java
@@ -9,15 +9,12 @@ import java.util.List;
 
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public record MessageUpdateDTO(
-        @NotNull(message = "Sender ID cannot be null")
-        Long senderId,
         @NotNull(message = "Channel id cannot be null")
         Long channelId,
         @NotNull(message = "Recipient id cannot be null")
         Long recipientId,
 
         @NotBlank(message = "Content cannot be blank")
-        String content,
-        ZonedDateTime timestamp,
-        Long threadParentId
+        String content
+
 ) {}
